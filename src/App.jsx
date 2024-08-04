d50df18c8c0111076b1bb5dd6280acefd0a7e105
Alejandro T. Naranjo <alejandrotnaranjo@proton.me>
2024-08-04 00:10:07 -0500

    changes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { PokemonProvider } from "./context/PokemonContext";
+import { Routes, Route } from "react-router-dom";
+import { Homepage, PokemonPage } from "./pages/pages";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonProvider>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="pokemon/:id" element={<PokemonPage />} />
+        </Routes>
+      </PokemonProvider>
+    </div>
+  );
+}
+
+export default App;
